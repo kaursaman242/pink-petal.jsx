23597d5e8f3ea46d61ecec1ae59768f442872d8b
Saman <kaursaman242@email.com>
2026-03-19 14:56:08 +0530

    feat: add About component with brand description

---
+++ pinkPetal-project/src/About.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const About = () => {
+  return (
+     <div className="px-4 py-10 text-center">
+      <h1 className="text-2xl md:text-3xl text-pink-500 font-semibold">About Us</h1>
+      <p className="mt-4 text-gray-600 max-w-xl mx-auto text-sm md:text-base">
+        Pink Petal Closet is a handmade clothing brand designed for girls who love soft aesthetics and elegant style.
+      </p>
+    </div>
+  )
+}
+
+export default About
